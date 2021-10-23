450c906210274cccce9b11b612c4aef3fe9d63a3
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-10-23 22:21:43 +0800

    Update jd_city.js

--- jd_city.js
+++ jd_city.js
@@ -26,7 +26,10 @@ if ($.isNode()) {
   cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let inviteCodes = []
+let inviteCodes = [
+  'RtGKzrihQw6hfYTPEtMy0A1vM1ibgTeCpYV6qYA0wNuelDPPLg',
+  'RtGKvYTEGVDkGsT6RK5bmqTc70BvSeKy4Dl4I-VmAg5ByvAS'
+]
 $.shareCodesArr = [];
 
 !(async () => {
