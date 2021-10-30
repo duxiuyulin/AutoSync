bd708a547387645abf91f7ceebcb8a7ea221821e
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-10-30 18:49:19 +0800

    Update jd_fcwb.js

--- jd_fcwb.js
+++ jd_fcwb.js
@@ -1,8 +1,8 @@
 //搞基大神-发财挖宝help助力
 //变量格式 
-//export fcwbinviteCode=''
-//export fcwbinviter=''
-//export fcwbroud=1  ##挖宝场次 1初级2中级3高级
+//export fcwbinviteCode=""
+//export fcwbinviter=""
+//export fcwbroud="1"  ##挖宝场次 1初级2中级3高级
 //运行一次即可看到助力马 直接输出的变量格式 直接复制在配置里
 //如果没有自动挖那就是写错了 自己手挖吧
 //入口：极速版 挖财寻宝
