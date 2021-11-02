14db229ad5391434613597555bfedfc3775806ac
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-11-02 20:31:18 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -5,6 +5,9 @@
 
 ##############短期活动##############
 
+# 手机竞猜 (不知道是什么鬼)
+6 0,7,17 * * * node /scripts/jd_sjjc.js >> /scripts/logs/jd_sjjc.log 2>&1
+
 # 双11特物
 3 9,13,16,19 2-8 11 * node /scripts/jd_1111superBrand.js >> /scripts/logs/jd_1111superBrand.log 2>&1
 
