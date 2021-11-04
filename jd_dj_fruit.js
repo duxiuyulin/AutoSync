5e4a16e2b49e8bbad966081ed0cb31336baa1be8
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-11-04 13:30:56 +0800

    Update jd_dj_fruit.js

--- jd_dj_fruit.js
+++ jd_dj_fruit.js
@@ -7,7 +7,7 @@ v5.6
 boxjs订阅地址:https://gitee.com/passerby-b/javascript/raw/master/JD/passerby-b.boxjs.json
 
 [task_local]
-10 0,3,8,11,17 * * * https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_fruit.js
+10 0,3,8,11,17 * * * jd_dj_fruit.js
 
 [Script]
 cron "10 0,3,8,11,17 * * *" script-path=https://raw.githubusercontent.com/passerby-b/JDDJ/main/jddj_fruit.js,tag=京东到家果园任务
