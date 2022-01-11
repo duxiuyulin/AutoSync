b1edd00d6bea0cd524741801e718afd25a7f53aa
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2022-01-11 15:49:05 +0800

    Update jd_cjhz.js

--- jd_cjhz.js
+++ jd_cjhz.js
@@ -113,13 +113,15 @@ async function main() {
             if (["BROWSE_SHOP"].includes($.oneTask.taskType) && $.oneTask.taskFinished === false) {
                 await apTaskDetail({ "taskId": $.oneTask.id, "taskType": $.oneTask.taskType, "channel": 4, "linkId": "Ll3Qb2mhCXSEWxruhv8qIw", "encryptPin": "7pcfSWHrAG9MKu3RKLl127VL5L4aIE1sZ1eRRdphpl8" });
                 await $.wait(1000)
-                for (let y = 0; y < ($.doList.status.finishNeed - $.doList.status.userFinishedTimes); y++) {
-                    $.startList = $.doList.taskItemList[y];
-                    $.itemName = $.doList.taskItemList[y].itemName;
-                    console.log(`去浏览${$.itemName}`)
-                    await apDoTask({ "taskId": $.allList[i].id, "taskType": $.allList[i].taskType, "channel": 4, "itemId": $.startList.itemId, "linkId": "Ll3Qb2mhCXSEWxruhv8qIw", "encryptPin": "7pcfSWHrAG9MKu3RKLl127VL5L4aIE1sZ1eRRdphpl8" })
-                    await $.wait(1000)
-                }
+                if ($.doList && $.doList.status) {
+                    for (let y = 0; y < ($.doList.status.finishNeed - $.doList.status.userFinishedTimes); y++) {
+                        $.startList = $.doList.taskItemList[y];
+                        $.itemName = $.doList.taskItemList[y].itemName;
+                        console.log(`去浏览${$.itemName}`)
+                        await apDoTask({ "taskId": $.allList[i].id, "taskType": $.allList[i].taskType, "channel": 4, "itemId": $.startList.itemId, "linkId": "Ll3Qb2mhCXSEWxruhv8qIw", "encryptPin": "7pcfSWHrAG9MKu3RKLl127VL5L4aIE1sZ1eRRdphpl8" })
+                        await $.wait(1000)
+                    }
+                } else return
             }
         }
     } else {
